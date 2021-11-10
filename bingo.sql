34d34248bf584a2a2bdf31f7309629fad1a3a94f
Moch. Fiqih Burhanuddin <16053028@users.noreply.github.com>
2021-11-10 16:00:13 +0700

    Update bingo.sql

--- bingo.sql
+++ bingo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 10, 2021 at 08:13 AM
+-- Generation Time: Nov 10, 2021 at 09:59 AM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 7.3.29
 
@@ -155,7 +155,91 @@ INSERT INTO `tbl_jawaban_soal` (`ID_JAWABAN_SOAL`, `ID_SOAL_PELAJARAN`, `TEKS_JA
 (13, 5, 'Memilih materi yang berguna', 0),
 (14, 5, 'Mengidentifikasi keadaan awal siswa', 0),
 (15, 5, 'Memilih model pembelajaran', 1),
-(16, 5, 'Meminta tanggapan siswa', 0);
+(16, 5, 'Meminta tanggapan siswa', 0),
+(17, 6, 'Tulisan artistic', 0),
+(18, 6, 'Mudah dipahami', 1),
+(19, 6, 'Berupa barang bekas', 0),
+(20, 6, 'Tidak mebutuhkan biaya dalam pembuatan', 0),
+(21, 7, 'Fungsi metode', 0),
+(22, 7, 'Fungsi evaluasi', 0),
+(23, 7, 'Fungsi media', 1),
+(24, 7, 'Fungsi model  ', 0),
+(25, 8, 'Manfaat media', 1),
+(26, 8, 'Manfaat evaluasi', 0),
+(27, 8, 'Manfaat metode', 0),
+(28, 8, 'Manfaat model', 0),
+(29, 9, 'Bahan dan peraga', 0),
+(30, 9, 'Peraga dan sarana', 0),
+(31, 9, 'Sarana, alat, audio dan peraga', 0),
+(32, 9, 'Guru, bahan, peraga dan saran prasarana sekolah', 1),
+(33, 10, 'Identifikasi kebutuhan siswa dan perumusan tujuan', 0),
+(34, 10, 'Evaluasi,identifikasi kebutuhan siswa dan perumusan tujuan', 0),
+(35, 10, 'Identifikasi kebutuhan siswa, perumusan tujuan-materi dan evaluasi', 0),
+(36, 10, 'Identifikasi kebutuhan siswa, perumusan tujuan-materi, perlibatan siswa dan evaluasi', 1),
+(37, 11, '(1), (2), (3) dan (5)', 0),
+(38, 11, '(1) dan (5)', 0),
+(39, 11, '(1),(2), dan (3)', 1),
+(40, 11, '(5) dan (6)', 0),
+(41, 13, 'Penyusunan rancangan media, dan penulisan naskah media', 1),
+(42, 13, 'Materi,tujuan, evaluasi', 0),
+(43, 13, 'Pengembangan materi, tujuan dan evaluasi', 0),
+(44, 13, 'Materi, buku pelajaran dan perpustakaan', 0),
+(45, 14, 'Radio, grafik dan chart.', 0),
+(46, 14, 'Demonstrasi, chart dan grafik.', 0),
+(47, 14, 'Televisi, radio, dan telepon.', 0),
+(48, 14, 'Grafik,chart dan peta.', 1),
+(49, 16, 'Film, video dan televisi', 1),
+(50, 16, 'Radio, televisi dan papan tulis.', 0),
+(51, 16, 'Grafik dan multimedia.', 0),
+(52, 16, 'Telepon dan alat perekam', 0),
+(53, 18, 'Media visual', 0),
+(54, 18, 'Media audio-visual', 0),
+(55, 18, 'Media cetak dan non cetak', 1),
+(56, 18, 'Media proyeksi', 0),
+(57, 20, 'Gambar dan papan tulis', 0),
+(58, 20, 'Proyektor transparasi dan film bingkai.', 1),
+(59, 20, 'Radio dan demonstrasi', 0),
+(60, 20, 'Wallsheets dan over head proyektor (ohp)', 0),
+(61, 21, 'Garis waktu', 0),
+(62, 21, 'Organisasi', 1),
+(63, 21, 'Klasifikasi', 0),
+(64, 21, 'Aliran', 0),
+(65, 23, 'Sudjana dan Rivai (1990)', 1),
+(66, 23, 'Daryanto (2009)', 0),
+(67, 23, 'Gagne (1987)', 0),
+(68, 23, 'Junaidi (2011)', 0),
+(69, 25, 'Media yang tampilannya dapat diamati dari arah pandang mana saja dan mempunyai dimensi panjang dan lebar', 0),
+(70, 25, 'Media yang tampilannya dapat diamati dari arah pandang mana saja dan mempunyai dimensi panjang, lebar, dan tinggi/tebal', 1),
+(71, 25, 'Media yang tampilannya dapat diamati dari  dua arah pandang saja dan mempunyai dimensi panjang, lebar, dan tinggi/tebal', 0),
+(72, 25, 'Media yang tampilannya dapat diamati dari arah pandang mana saja dan mempunyai dimensi.', 0),
+(73, 26, 'Media visual', 0),
+(74, 26, 'Media audio visual', 1),
+(75, 26, 'Media audio', 0),
+(76, 26, 'Media transparansi', 0),
+(77, 27, 'Buku pelajaran, ensiklopedi, modul, rekaman', 0),
+(78, 27, 'Kaset audio, modul, buku, slide presentasi', 0),
+(79, 27, 'Kaset audio, rekaman, Slide presentasi, Gambar/foto, Video, rekaman dari TV, Video CD/DVD, dll', 1),
+(80, 27, 'Koran, gambar/foto, buku pelajaran dll', 0),
+(81, 29, 'Memberikan pengalaman belajar yang bervariasi', 0),
+(82, 29, 'Memenuhi minat siswa yang bervariasi', 0),
+(83, 29, 'Membentuk sikap siswa', 0),
+(84, 29, 'Memperlihatkan objek yang terlalu kecil atau besar', 1),
+(85, 30, 'Study tour', 1),
+(86, 30, 'Audio', 0),
+(87, 30, 'Visual diam', 0),
+(88, 30, 'Visual gerak', 0),
+(89, 31, 'Study tour', 0),
+(90, 31, 'Audio visual diam', 0),
+(91, 31, 'Obyek fisik', 1),
+(92, 31, 'Visual gerak  ', 0),
+(93, 32, 'Karakteristik media', 0),
+(94, 32, 'Tujuan pembelajaran', 1),
+(95, 32, 'Karakteristik siswa', 0),
+(96, 32, 'Situasi dan kondisi kelas', 0),
+(97, 33, 'Psikologi pendidikan.', 0),
+(98, 33, 'Psikologi perkembangan dan psikologi belajar.', 1),
+(99, 33, 'Psikologi perkembangan anak', 0),
+(100, 33, 'Psikologi umum.', 0);
 
 -- --------------------------------------------------------
 
@@ -279,11 +363,31 @@ CREATE TABLE `tbl_soal_pelajaran` (
 --
 
 INSERT INTO `tbl_soal_pelajaran` (`ID_SOAL_PELAJARAN`, `ID_PELAJARAN`, `TEKS_SOAL_PELAJARAN`) VALUES
-(1, 1, 'Coba soal ini ya?'),
 (2, 2, 'Media yang memiliki fungsi utama untuk menurunkan keabstrakan konsep sering disebut?'),
 (3, 2, 'Berikut ini merupakan kelemahan media cetak adalah ?'),
 (4, 2, 'Beberapa hal yang perlu diperhatikan guru dalam menggunakan media pembelajaran untuk mempertinggi kualitas pembelajaran adalah?'),
-(5, 2, 'Menurut model ASSURE, sebelum memilih media, guru perlu …');
+(5, 2, 'Menurut model ASSURE, sebelum memilih media, guru perlu …'),
+(6, 2, 'Prinsip kesederhanaan dalam pembuatan media adalah…'),
+(7, 2, 'Menghadirkan obyek sebenarnya dan obyek yang langkah,membuat duplikasi dari obyek yang sebenarnya, membuat konsep abstrak ke konsep konkret, memberi kesamaan persepsi, mengatasi hambatran waktu, tempat, jumlah, dan jarak, menyajikan ulang informasi secara konsisten dan memberi suasana yang belajar yang tidak tertekan, santai, dan menarik. merupakan...'),
+(8, 2, 'Penyampaian materi pelajaran dapat diseragamkan, proses pembelajaran menjadi lebih jelas dan menarik, proses pembelajaran menjadi lebih interaktif, efisiensi dalam waktu dan tenaga, meningkatkan kualitas hasil belajar siswa, media memungkinkan proses belajar dapat dilakukan dimana saja dan kapan saja, media dapat menumbuhkan sikap positif siswa terhadap materi dan proses belajar, merubah peran guru ke arah yang lebih positif dan produktif. ...'),
+(9, 2, 'Apa saja ruang lingkup media pembelajaran....'),
+(10, 2, 'Apa saja prosedur dalam memilih media pembelajaran...'),
+(11, 2, '<ol><li>kesesuaian dengan tujuan.</li><li>ketepatgunaan.</li><li>keadaan peserta didik.</li><li>ketersedian.</li><li>kesempurnaan.</li><li>fleksibilitas</li></ol><p>Manakah yang menjadi kriteria dalam pemilihan media pembelajaran...</p>'),
+(13, 2, 'Sebagai berikut, manakah prosedur dalam merancang media...'),
+(14, 2, '<p class=\"MsoListParagraph\" style=\"margin-top:0cm;margin-right:0cm;margin-bottom:\r\n0cm;margin-left:18.0pt;mso-add-space:auto;text-align:justify;text-indent:-18.0pt;\r\nmso-list:l0 level1 lfo1\"><span lang=\"IN\" style=\"font-size:12.0pt;mso-bidi-font-size:11.0pt;line-height:107%;\r\nfont-family:&quot;Times New Roman&quot;,serif\">Manakah yang termaksud jenis media\r\nvisual...<o:p></o:p></span></p>'),
+(16, 2, '<p class=\"MsoListParagraph\" style=\"margin-top:0cm;margin-right:0cm;margin-bottom:\r\n0cm;margin-left:18.0pt;mso-add-space:auto;text-align:justify;text-indent:-18.0pt;\r\nmso-list:l0 level1 lfo1\"><span lang=\"IN\" style=\"font-size:12.0pt;mso-bidi-font-size:11.0pt;line-height:107%;\r\nfont-family:&quot;Times New Roman&quot;,serif\">Manakah yang termaksud jenis media\r\naudio-visual...<o:p></o:p></span></p>'),
+(18, 2, '<p><span lang=\"IN\" style=\"font-size:12.0pt;mso-bidi-font-size:\r\n11.0pt;line-height:107%;font-family:&quot;Times New Roman&quot;,serif;mso-fareast-font-family:\r\nCalibri;mso-fareast-theme-font:minor-latin;mso-ansi-language:IN;mso-fareast-language:\r\nEN-US;mso-bidi-language:AR-SA\">Apa saja yang termaksud media yang ditinjau dari\r\npenggunaannya...</span><br></p>'),
+(20, 2, '<p class=\"MsoListParagraph\" style=\"margin-top:0cm;margin-right:0cm;margin-bottom:\r\n0cm;margin-left:18.0pt;mso-add-space:auto;text-align:justify;text-indent:-18.0pt;\r\nmso-list:l0 level1 lfo1\"><span lang=\"IN\" style=\"font-size:12.0pt;mso-bidi-font-size:11.0pt;line-height:107%;\r\nfont-family:&quot;Times New Roman&quot;,serif\">Manakah yang termaksud jenis media\r\nproyeksi...<o:p></o:p></span></p>'),
+(21, 2, 'Pak Ardi guru IPS kelas IV SD, pada saat\r\ntertentu akan membahas pokok bahasan struktur kecamatan. Agar murid-muridnya\r\ndapat cepat memahami materi yang dijelaskan sebaiknya menggunakan bagan ...'),
+(23, 2, '<p>Media pembelajaran adalah segala sesuatu\r\nyang digunakan untuk menyalurkan pesan serta dapat merangsang pikiran,\r\nperasaan, perhatian, dan kemauan si belajar sehingga dapat mendorong terjadinya\r\nproses belajar”.&nbsp;Menurut tokoh pendidikan ?</p>'),
+(25, 2, '<p class=\"MsoListParagraph\" style=\"margin-top:0cm;margin-right:0cm;margin-bottom:\r\n0cm;margin-left:18.0pt;mso-add-space:auto;text-align:justify;text-indent:-18.0pt;\r\nmso-list:l0 level1 lfo1\"><span lang=\"IN\" style=\"font-size:12.0pt;mso-bidi-font-size:11.0pt;line-height:107%;\r\nfont-family:&quot;Times New Roman&quot;,serif\">Media pembelajaran tiga dimensi adalah . .\r\n.<o:p></o:p></span></p>'),
+(26, 2, '<p class=\"MsoListParagraph\" style=\"margin-top:0cm;margin-right:0cm;margin-bottom:\r\n0cm;margin-left:18.0pt;mso-add-space:auto;text-align:justify;text-indent:-18.0pt;\r\nmso-list:l0 level1 lfo1\"><span lang=\"IN\" style=\"font-size:12.0pt;mso-bidi-font-size:11.0pt;line-height:107%;\r\nfont-family:&quot;Times New Roman&quot;,serif\">Media power point termasuk kedalam jenis\r\nmedia. . .&nbsp;<o:p></o:p></span></p>'),
+(27, 2, '<p class=\"MsoListParagraph\" style=\"margin-top:0cm;margin-right:0cm;margin-bottom:\r\n0cm;margin-left:18.0pt;mso-add-space:auto;text-align:justify;text-indent:-18.0pt;\r\nmso-list:l0 level1 lfo1\"><span lang=\"IN\" style=\"font-size:12.0pt;mso-bidi-font-size:11.0pt;line-height:107%;\r\nfont-family:&quot;Times New Roman&quot;,serif\">Manakah dibawah ini yang termasuk kedalam\r\nmedia noncetak ?<o:p></o:p></span></p>'),
+(29, 2, '<p>Dalam mata pelajaran IPA, guru tidak saja\r\nmenjelaskan tentang tumbuhan tetapi juga menunjukkan tanaman asli yang dibawa\r\nke kelas. Membawa tanaman ke kelas dalam pembelajaran IPA mempunyai manfaat . .</p>'),
+(30, 2, '<p><span lang=\"IN\" style=\"font-size:12.0pt;mso-bidi-font-size:\r\n11.0pt;line-height:107%;font-family:&quot;Times New Roman&quot;,serif;mso-fareast-font-family:\r\nCalibri;mso-fareast-theme-font:minor-latin;mso-ansi-language:IN;mso-fareast-language:\r\nEN-US;mso-bidi-language:AR-SA\">Media Yang Menggunakan Pembelajaran Langsung Ke\r\nObyek Atau Tempat Study Seperti Museum, Candi Merupakan Media Pembelajaran...</span><br></p>'),
+(31, 2, '<p class=\"MsoListParagraph\" style=\"margin-top:0cm;margin-right:0cm;margin-bottom:\r\n0cm;margin-left:18.0pt;mso-add-space:auto;text-align:justify;text-indent:-18.0pt;\r\nmso-list:l0 level1 lfo1\"><span lang=\"IN\" style=\"font-size:12.0pt;mso-bidi-font-size:11.0pt;line-height:107%;\r\nfont-family:&quot;Times New Roman&quot;,serif\">Media yang menggunakan benda nyata, model,\r\nspecimen merupakan media pembelajaran...<o:p></o:p></span></p>'),
+(32, 2, '<p><span lang=\"IN\" style=\"font-size:12.0pt;mso-bidi-font-size:\r\n11.0pt;line-height:107%;font-family:&quot;Times New Roman&quot;,serif;mso-fareast-font-family:\r\nCalibri;mso-fareast-theme-font:minor-latin;mso-ansi-language:IN;mso-fareast-language:\r\nEN-US;mso-bidi-language:AR-SA\">Untuk membantu siswa menguasai kemampuan\r\nmenggambar berbagai bangun datar, guru menggunakan poster berbagai gambar\r\nbangun datar dalam kegiatan pembelajaran. Pemilihan media tersebut didasarkan pada\r\nkesesuaian dengan ...&nbsp;</span><br></p>'),
+(33, 2, '<p class=\"MsoListParagraph\" style=\"margin-top:0cm;margin-right:0cm;margin-bottom:\r\n0cm;margin-left:18.0pt;mso-add-space:auto;text-indent:-18.0pt;mso-list:l0 level1 lfo1\"><span lang=\"IN\" style=\"font-size:12.0pt;\r\nmso-bidi-font-size:11.0pt;line-height:107%;font-family:&quot;Times New Roman&quot;,serif\">Dalam\r\nlandasan psikologi&nbsp;&nbsp;apa saja yang mendasari media pembelajaran,\r\nsebagai berikut...<o:p></o:p></span></p>');
 
 -- --------------------------------------------------------
 
@@ -511,7 +615,7 @@ ALTER TABLE `tbl_instansi_pendidikan`
 -- AUTO_INCREMENT for table `tbl_jawaban_soal`
 --
 ALTER TABLE `tbl_jawaban_soal`
-  MODIFY `ID_JAWABAN_SOAL` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `ID_JAWABAN_SOAL` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=101;
 
 --
 -- AUTO_INCREMENT for table `tbl_kartu_bingo`
@@ -547,7 +651,7 @@ ALTER TABLE `tbl_ruang`
 -- AUTO_INCREMENT for table `tbl_soal_pelajaran`
 --
 ALTER TABLE `tbl_soal_pelajaran`
-  MODIFY `ID_SOAL_PELAJARAN` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `ID_SOAL_PELAJARAN` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- AUTO_INCREMENT for table `tbl_status_instansi`
